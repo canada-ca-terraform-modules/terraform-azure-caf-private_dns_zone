b74fa165ea7a0ab0e2211c7dd2f17c1ad9e02d5c
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2024-07-24 14:28:02 -0400

    Added locals value tracking private dns zone ids to not break ESLZ template

--- ESLZ/private-dns-zone.tf
+++ ESLZ/private-dns-zone.tf
@@ -3,8 +3,15 @@ variable "privateDNSzone" {
   default = {}
 }
 
+locals {
+  private_dns_zone_ids = {
+    for name, param in try(var.privateDNSzone, {}) :
+    name => module.private_dns_zone[name].private_dns_zone_id
+  }
+}
+
 module "private_dns_zone" {
-  source = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-private_dns_zone.git?ref=v1.0.0"
+  source = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-private_dns_zone.git?ref=v1.0.1"
   for_each = var.privateDNSzone
 
   private_dns_zone = each.value
